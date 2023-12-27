2e349c462da07ea05e53edbb4f941fdbc7838d1b
Nyat Ghirmazion <nyat@Nyats-MacBook-Air.local>
2023-12-27 16:30:21 +0100

    antecknings js, + localstorage

---
+++ js/notes.js
@@ -0,0 +1,16 @@
+const noteArea = document.getElementById("input-area");
+
+function saveNoteToLocalStorage() {
+    localStorage.setItem("userNote", noteArea.value);
+}
+
+noteArea.addEventListener('input', function () {
+    saveNoteToLocalStorage(); 
+});
+
+window.addEventListener('load', function () {
+    const savedNote = localStorage.getItem('userNote');
+    if (savedNote) {
+        noteArea.value = savedNote;
+    }
+});
