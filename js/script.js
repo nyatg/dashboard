5c19504c8f8643b191050a6db2bebf1f22e9ff3a
Nyat Ghirmazion <nyat@Nyats-MacBook-Air.local>
2023-12-29 12:55:54 +0100

    mindre ändringar i koden, lagt in möjlighet till att byta bg, tillfällig funktionalitet

--- js/script.js
+++ js/script.js
@@ -21,3 +21,46 @@ function updateTimeAndDate() {
 updateTimeAndDate();
 
 setInterval(updateTimeAndDate, 60000);
+
+// const bgBtn = document.getElementById('bg-btn');
+
+// bgBtn.addEventListener('click', function () {
+//     let randomNr = Math.floor(Math.random() * 5) + 1;
+
+//     console.log(randomNr);
+    
+//     if (randomNr == 1) {
+//         document.body.style.backgroundColor = 'green';
+//     } else if (randomNr == 2) {
+//         document.body.style.backgroundColor = 'blue';
+//     } else {
+//         document.body.style.backgroundImage = 'yellow';
+//     }
+// });
+
+// const bgBtn = document.getElementById('bg-btn');
+// bgBtn.addEventListener('click', function () {
+//     let randomNr = Math.floor(Math.random() * 5) + 1;
+//     let imgUrl;
+
+//     switch (randomNr) {
+//         case 1:
+//             imageUrl = 'url()';
+//             break;
+//         case 2:
+//             imageUrl = 'url()';
+//             break;
+//         case 3:
+//             imageUrl = 'url()';
+//             break;
+//         case 4:
+//             imageUrl = 'url()';
+//             break;
+//         case 5:
+//             imageUrl = 'url()';
+//         default:
+//             imgUrl = 'url()';
+//             break;
+//     }
+//     document.body.style.backgroundImage = imgUrl;
+// });
